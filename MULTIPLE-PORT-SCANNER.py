e6e45716c2d77fb15c0e8409804b0eda16e39275
ANIKET BHAGWATE <aniketbhagwate@gmail.com>
2019-01-10 01:47:18 +0530

    Add files via upload

---
+++ MULTIPLE-PORT-SCANNER.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# libraries 
+import os
+import socket
+os.system("pip install colorama==0.3.3")
+from colorama import Fore, Back, Style
+os.system("clear")
+print(Fore.GREEN + Style.BRIGHT + "")  	
+print("		 __  __ _   _ _   _____ ___ ____  _     _____      ____   ___  ____ _____ ") 
+print("		|  \/  | | | | | |_   _|_ _|  _ \| |   | ____|    |  _ \ / _ \|  _ \_   _| ")
+print("		| |\/| | | | | |   | |  | || |_) | |   |  _| _____| |_) | | | | |_) || |  ")
+print("		| |  | | |_| | |___| |  | ||  __/| |___| |__|_____|  __/| |_| |  _ < | |  ")
+print("		|_|  |_|\___/|_____|_| |___|_|   |_____|_____|    |_|    \___/|_| \_\|_|   ")                                                                      
+print("		 ____   ____    _    _   _ _   _ _____ ____   ")
+print("		/ ___| / ___|  / \  | \ | | \ | | ____|  _ \  ")
+print("		\___ \| |     / _ \ |  \| |  \| |  _| | |_) | ")
+print("		 ___) | |___ / ___ \| |\  | |\  | |___|  _ <  ")
+print("		|____/ \____/_/   \_\_| \_|_| \_|_____|_| \_\ ")                                            
+print("		--------------------------------------------------------------------")
+print("                                                     [ ~NULLBYTE007]")
+print("		--------------------------------------------------------------------")
+host_name = socket.gethostname()
+host_ip_address = socket.gethostbyname(host_name)
+print(Fore.CYAN + Style.BRIGHT + "")
+ip_range = input("Input the Port range for Scan\nExample : 100-200\n  -->> ")
+print(Fore.GREEN + Style.BRIGHT + "")
+ip_range = ip_range.split("-")
+for x in range(int(ip_range[0]),int(ip_range[1])):
+	try:
+		print("\nHost Name : " + str(host_name) + " Has Ip address : " + str(host_ip_address) + " Has -->>  \"" + socket.getservbyport(x) + "\" <<-- Running on Port : " + str(x))
+	except:
+		pass
+		
+	if x != 100:	
+		continue	
+	
---
+++ PORT-SERVICE-IDENTIFIER.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import socket
+import os
+os.system("pip install colorama==0.3.3")
+from colorama import Fore, Back, Style
+os.system("clear")
+print(Fore.GREEN + Style.BRIGHT + "") 
+print("		 ____   ___  ____ _____   ____  _____ ______     _____ ____ _____  ")
+print("		|  _ \ / _ \|  _ \_   _| / ___|| ____|  _ \ \   / /_ _/ ___| ____| ")
+print("		| |_) | | | | |_) || |   \___ \|  _| | |_) \ \ / / | | |   |  _|  ")
+print("		|  __/| |_| |  _ < | |    ___) | |___|  _ < \ V /  | | |___| |___ ")
+print("		|_|    \___/|_| \_\|_|   |____/|_____|_| \_\ \_/  |___\____|_____|  ")                                                         
+print("		 ___ ____  _____ _   _ _____ ___ _____ ___ _____ ____   ")
+print("		|_ _|  _ \| ____| \ | |_   _|_ _|  ___|_ _| ____|  _ \  ")
+print("		 | || | | |  _| |  \| | | |  | || |_   | ||  _| | |_) | ")
+print("		 | || |_| | |___| |\  | | |  | ||  _|  | || |___|  _ <  ")
+print("		|___|____/|_____|_| \_| |_| |___|_|   |___|_____|_| \_\ ")
+print("		--------------------------------------------------------------------")
+print("                                                     [ ~NULLBYTE007]")
+print("		--------------------------------------------------------------------")
+print("\n")
+print(Fore.CYAN + Style.BRIGHT + "")
+port_input = input("INPUT PORT NUMBERS : ")
+print(Fore.GREEN + Style.BRIGHT + "")
+port_input = port_input.split(" ")
+print(port_input)
+print("--------------------------------------------------------------------------------")	
+for (count,x) in enumerate(port_input) :
+	try:
+		print("\n[*]Service Running on -->> " + str(x) + " is : \" " + socket.getservbyport(int(x)) + "\"")
+		#print(count)   Use this to just count the actual count going on 
+	except:
+		print("\n!!! [*]No Service exists on PORT !!! : --> " + str(x))
+		pass
+	
+	if count > len(port_input):
+		break
+	print("--------------------------------------------------------------------------------")	
