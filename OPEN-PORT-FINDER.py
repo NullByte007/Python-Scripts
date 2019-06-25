6e00f34e0247f62a0951916ddf430774d40c23e1
ANIKET BHAGWATE <aniketbhagwate@gmail.com>
2019-06-25 10:24:38 +0530

    Update OPEN-PORT-FINDER.py

--- OPEN-PORT-FINDER.py
+++ OPEN-PORT-FINDER.py
@@ -15,8 +15,6 @@
 #libraries
 import os
 import socket
-print("[*] Downloading Required Library...")
-os.system("pip install colorama==0.3.3")
 from colorama import Fore, Back, Style
 os.system("clear")
 print(Fore.GREEN + Style.BRIGHT + "") 
